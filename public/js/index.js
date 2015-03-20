887a0dff6b8cfe508c096d101e7ff08deaeafb59
Boby <zsb0213@gmail.com>
2015-03-20 13:10:36 +0800

    add donate.html -- unfinished

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,70 @@
 ﻿
-console.log('haha');
+var pageTitle = document.title;
 
-var submit = document.getElementById('login-buttom');
-var tip = document.getElementById('login-tip');
+switch (pageTitle) {
+    case 'Continue-login':
+        handleLoginPage();
+        break;
+    case 'Continue-donate':
+        handleDonatePage();
+        break;
+        
+}
 
+//
+// work for login.html
+//
+function handleLoginPage() {
 
+    var submit = document.getElementById('login-buttom');
+    var tip = document.getElementById('login-tip');
 
-submit.onclick = function () {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+    submit.onclick = function () {
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
 
-    if (username == '' || password == '') {
-        tip.innerText = '请输入账号、密码。';
-        tip.style.display = 'block';
-        return;
-    }
-
-    var fd = new FormData();
-    fd.append('member_id', username);
-    fd.append('password', password);
-    
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        var responJSON = JSON.parse(xhr.responseText);
-        if (responJSON.errcode == 1) {
-            tip.innerText = '账号或密码错误！';
+        if (username == '' || password == '') {
+            tip.innerText = '请输入账号、密码。';
             tip.style.display = 'block';
+            return;
+        }
+
+        var fd = new FormData();
+        fd.append('member_id', username);
+        fd.append('password', password);
+
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            var responJSON = JSON.parse(xhr.responseText);
+            if (responJSON.errcode == 1) {
+                tip.innerText = '账号或密码错误！';
+                tip.style.display = 'block';
+            }
+        }
+
+        xhr.open('post', '/auth/login', true);
+        xhr.send(fd);
+
+    }
+}
+
+//
+//
+//
+function handleDonatePage() {
+
+    var isbnBox = document.getElementById('search-box');
+
+    isbnBox.onfocus = function () {
+        // lister to Enter when focus on search-box 
+        this.onkeydown = function (event) {
+            var e = event || window.event;
+
+            if (e && e.keyCode != 13) return;
+            else {
+                alert("hah")
+            }
         }
     }
-
-    xhr.open('post', '/auth/login', true);
-    xhr.send(fd);
-
 }
+
