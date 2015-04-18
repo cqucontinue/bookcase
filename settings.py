fc349a30eac79d2e594392868960ba03271bea9d
reimondo <reimondo.zero@gmail.com>
2015-04-18 17:40:27 +0800

    Add settings file and logs file

---
+++ settings.py
@@ -0,0 +1,36 @@
+import os
+
+# The following settings are mainly for tornado's
+# internal settings
+
+# Tornado debug setting
+DEBUG = True
+
+# Security about
+XSRF_COOKIES = False
+
+# Login url for auto jump
+LOGIN_URL="/auth/nologin"
+
+# Set file path
+DIRNAME = os.path.dirname(__file__)
+STATIC_PATH = os.path.join(DIRNAME, "public")
+STATIC_URL_PREFIX = "/static/"
+TEMPLATE_PATH = os.path.join(DIRNAME, "public/template")
+
+
+# Some other settings
+# Set log file prefix
+LOG_FILE_PREFIX = os.path.join(DIRNAME, "logs/tornado.log")
+
+# Set http server
+LISTEN_PORT = 8000
+LISTEN_ADDRESS = "127.0.0.1"
+
+# Set mongodb
+MONGODB_HOST = "127.0.0.1"
+MONGODB_PORT = 27017
+DB_CONTINUE = "continue"
+COLL_BOOKS = "books"
+COLL_MEMBERS = "members"
+COLL_WUNDER = "wunbooks"
